2a74807055cf97317083b2fd3ab98007194b391b
hohee-hee <122425990+hohee-hee@users.noreply.github.com>
2023-01-22 14:48:10 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.45 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n+1)/2];
+        int idx = 0;
+        for(int i = 1 ; i <= n ; i ++){
+            if(i%2 != 0)
+                answer[idx++] = i;
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
