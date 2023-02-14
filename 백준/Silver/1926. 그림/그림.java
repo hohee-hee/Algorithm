febffb888bf7ff5d8589ba6f5dfb95a8c8abc75b
hohee-hee <122425990+hohee-hee@users.noreply.github.com>
2023-02-14 09:05:29 +0900

    [Silver I] Title: 그림, Time: 392 ms, Memory: 39096 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,67 +1,79 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());  
-    	
-    	int N = Integer.parseInt(st.nextToken());
-    	int M = Integer.parseInt(st.nextToken());
-    	
-    	//입력값 넣어주기
-    	int[][] pic = new int[N][M];    	
-    	for(int i = 0 ; i < N ; i++) {
-    		StringTokenizer board = new StringTokenizer(br.readLine());
-    		for(int j = 0 ; j < M ; j++) {
-    			pic[i][j] = Integer.parseInt(board.nextToken());
-    		}
-    	}
-    	
-    	//방문 여부를 확인할 변수
-    	boolean[][] visit = new boolean[N][M];
-    	int max = 0; //가장 넓은 그림의 넓이를 저장할 변수
-   
-    	
-
-    	int[] dx = { 0 , 0, -1, 1};
-    	int[] dy = { -1, 1, 0, 0 };
-
-    	int cnt = 0; //그림의 개수
-    	for(int i = 0 ; i < N ; i++) {
-    		for(int j = 0 ; j < M ; j++) {
-    			if(visit[i][j] == true || pic[i][j] == 0) { ;continue; }
-    			int tmp = 0;
-    			cnt++;
-    	        Queue<Integer> q_n = new LinkedList<>();
-    	        Queue<Integer> q_m = new LinkedList<>();
-    			q_n.offer(i);
-    			q_m.offer(j);                
-    			visit[i][j] = true;
-    			while(!(q_m.isEmpty())) {
-    	    		tmp++;
-    	    		int cx = q_n.poll();
-    	    		int cy = q_m.poll();
-    	    		for(int k = 0 ; k < 4 ; k++) {    	    			
-    	    			int nx = cx + dx[k];
-    	    			int ny = cy + dy[k];
-    	    			if(nx < 0 || ny < 0 || nx >= N || ny >= M) { continue; } //범위 벗어나면 pass
-    	    			if(visit[nx][ny] || pic[nx][ny] == 0) { continue; } //방문했거나 0이면
-    	    			q_n.offer(nx);
-    	    			q_m.offer(ny);    			
-    	    			visit[nx][ny] = true;
-    	    		}
-    	    	}    			
-    			if(max<tmp)
-    				max = tmp;    			
-    		}
-    	}
-    	
-    	System.out.println(cnt);
-    	System.out.println(max);    	
-    }   
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// n, m짜리 배열 만들기
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][] map = new int[n][m];
+		
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<m; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+				
+		// dx, dy 4방탐색 배열 만들기
+		int[] dx = {-1, 1, 0, 0};
+		int[] dy = {0, 0, -1, 1};
+		
+		// queue 만들기
+		Queue<int[]> queue = new ArrayDeque<>();
+		
+		
+		// 그림의 개수 = cnt / 그림의 크기 = size
+		int cnt = 0; int max_size = 0;
+		// x, y가 1인 지점을 만나면 0으로 바꾸고 queue에 x, y를 넣음 -> number +1 ; count는 0으로 바꿀때마다 ++;
+		for(int x=0; x<n; x++) {
+			for(int y=0; y<m; y++) {
+				if(map[x][y] == 1) {
+					cnt++;
+					int size = 1;
+					map[x][y] = 0;
+					
+					int[] array = new int[2];
+					array[0] = x; 
+					array[1] = y;
+					queue.offer(array);
+					
+					// queue에서 꺼낸 cx, cy에서 cx+dx, cy+dy 중 1을 만나면 0으로 바꾸고 queue에 해당 값 넣음 (queue가 빌 때까지)
+					while(!queue.isEmpty()) {
+						array = queue.poll();
+						int cx = array[0];
+						int cy = array[1];
+						for(int i=0; i<4; i++) {
+							if(cx+dx[i] < 0 || cy+dy[i] < 0 || cx+dx[i] >= n || cy+dy[i] >= m) continue;
+							if(map[cx+dx[i]][cy+dy[i]] == 1) {
+								map[cx+dx[i]][cy+dy[i]] = 0;
+								size++;
+								
+                                // 새로 선언 안해주면 참조형이어서 다른 값들도 바뀜
+                                // 초기화 필수
+								array = new int[2];
+								array[0] = cx+dx[i]; 
+								array[1] = cy+dy[i];
+								queue.offer(array);
+							}
+						}
+						
+					}
+					if(size > max_size) {
+						max_size = size;
+					}
+				}
+			}
+		}
+		if(cnt != 0) System.out.println(cnt + "\n" + max_size);
+		else System.out.println(0 + "\n" + 0);
+		
+	}
 }
