d1cf59026b10c77824cf92a8f8c52689e6c31049
hohee-hee <122425990+hohee-hee@users.noreply.github.com>
2023-03-05 16:47:20 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 160 ms, Memory: 24,012 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
@@ -1,62 +1,46 @@
 import java.util.Scanner;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-        
+public class Solution {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{		
-			//입력 받기
+		
+		for(int tc = 1 ; tc <= T ; tc++) {
 			int N = sc.nextInt();
 			int K = sc.nextInt();
-			int[][] board = new int[N][N]; //퍼즐판
+			int[][] puzzle = new int[N][N];
 			for(int i = 0 ; i < N ; i++) {
 				for(int j = 0 ; j < N ; j++) {
-					board[i][j] = sc.nextInt();
+					puzzle[i][j] = sc.nextInt();
 				}
 			}
 			
-			//연속된 1의 개수 체크하기
-			int len_x = 0; //가로 길이 체크
-			int len_y = 0; //세로 길이 체크
-			int answer = 0; //정답 저장
+			int con_r = 0;
+			int con_c = 0;
+			int answer =0;
 			
 			for(int i = 0 ; i < N ; i++) {
-				//초기화
-				len_x = 0; 
-				len_y = 0;
+				con_r = 0;
+				con_c = 0;
 				for(int j = 0 ; j < N ; j++) {
-					//길이 확인
-					//만약 현재 위치의 퍼즐판에 0이 있다면 현재 저장된 길이가 K와 같은지 확인 
-					// -> 같다면 정답에 1 더하기
-					// 그 후에 길이를 0으로 초기화
-					//만약 현재 위치의 퍼즐판에 1이 있다면 길이에 1 더해주기	
-					
+					if(puzzle[i][j] == 1) {	con_r++; }	
+					else if(puzzle[i][j] == 0) { 
+						if(con_r == K) { answer++; }
+						con_r = 0; 
+					}
 
-					//가로 길이 확인
-					if(board[i][j] == 0) {
-						if(len_x == K) { answer++; }
-						len_x = 0;
-					}
-					else { len_x++; }
 					
-					//세로 길이 확인
-					if(board[j][i] == 0) {
-						if(len_y == K) { answer++; }
-						len_y = 0;
+					if(puzzle[j][i] == 1) {	con_c++; }
+					else if(puzzle[j][i] == 0) { 
+						if(con_c == K) { answer++; }
+						con_c = 0; 
 					}
-					else { len_y++; }
 				}
-                
-				if(len_x == K) { answer++; }
-				if(len_y == K) { answer++; }
+				if(con_r == K) { answer++; }
+				if(con_c == K) { answer++; }
 			}
 			
-			System.out.printf("#%d %d\n", test_case, answer);
+			System.out.printf("#%d %d\n", tc, answer);
 		}
 	}
 }
