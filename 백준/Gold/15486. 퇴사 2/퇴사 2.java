25e7b32820031fbe09b34adaa59946a29d99ced4
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2023-08-06 22:42:20 +0900

    [Gold V] Title: 퇴사 2, Time: 768 ms, Memory: 337404 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,49 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{		//1. 입력받기
+
+        
+        // 1. 입력받기
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+        
+		int[][] schedule = new int[N+1][2]; 
+
+		for(int i = 1 ; i <= N ; i++) {
+
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			schedule[i][0] = Integer.parseInt(st.nextToken());
+
+			schedule[i][1] = Integer.parseInt(st.nextToken());
+
+		}
+
+		
+
+		// 2. dp
+
+		int[] dp = new int[N+2];
+
+		for(int i = N ; i > 0 ; i--) {
+
+			if(schedule[i][0] + i > N+1) dp[i] = dp[i+1];
+
+			else dp[i] = Math.max(dp[i+schedule[i][0]] + schedule[i][1], dp[i+1]);
+
+		}
+
+        
+        // 3. 출력
+        
+		System.out.println(dp[1]);
+
+	}
+
+}
