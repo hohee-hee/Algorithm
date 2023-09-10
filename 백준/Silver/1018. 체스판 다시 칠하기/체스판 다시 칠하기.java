3f25e5e233760a4f9f5fd6c958c021fae1d66d70
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2023-09-10 15:41:23 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 88 ms, Memory: 12264 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        char[][] board = new char[N][M];
+        for (int r = 0; r < N; r++) {
+            String str = br.readLine();
+            for (int c = 0; c < M; c++) {
+                board[r][c] = str.charAt(c);
+            }
+        }
+
+        char[][] white = {
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}
+        };
+        char[][] black = {
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'}
+        };
+        int answer = Integer.MAX_VALUE;
+
+        for (int r = 0; r <= N-8 ; r++) {
+            for (int c = 0; c <= M-8 ; c++) {
+
+                char start = board[r][c];
+
+                int wchange = 0;
+                int bchange = 0;
+                for (int i = 0; i < 8; i++) {
+                    for (int j = 0; j < 8; j++) {
+                        if(board[r+i][c+j] != white[i][j]) wchange++;
+                        if(board[r+i][c+j] != black[i][j]) bchange++;
+                    }
+                }
+
+                answer = Math.min(Math.min(wchange, bchange), answer);
+            }
+
+        }
+
+        System.out.println(answer);
+    }
+}
