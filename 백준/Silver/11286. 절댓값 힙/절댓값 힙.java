14028e21bc057fc3514a7e4f55e71888387d6e52
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2023-09-25 22:55:42 +0900

    [Silver I] Title: 절댓값 힙, Time: 284 ms, Memory: 24376 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if(Math.abs(o1) == Math.abs(o2)) return o1 - o2;
+                return Math.abs(o1) - Math.abs(o2);
+            }
+        });
+        for(int i = 0 ; i < N ; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if(x == 0) {
+                if(!pq.isEmpty()) sb.append(pq.poll());
+                else sb.append(0);
+                sb.append("\n");
+            } else {
+                pq.offer(x);
+            }
+        }
+
+
+        System.out.println(sb);
+    }
+}
