5a96ae12b3896d42f6fd234e35e6efbe1c8788a1
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2023-11-21 23:11:42 +0900

    [Gold IV] Title: 감시, Time: 288 ms, Memory: 63768 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -1,177 +1,141 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	static int N, M, answer;
-	static int[][] office; //사무실 맵
-	static ArrayList<int[]> cctv;
-	
-	static int[] dr = {0,-1,0,1};
-	static int[] dc = {-1,0,1,0};
-	
-	public static void main(String[] args) throws IOException {
-		//1. 입력 및 초기화
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		// - 사무실 정보 입력받기
-		cctv = new ArrayList<>(); //cctv 정보
-		office = new int[N][M];
-		for(int r = 0 ; r < N ; r++) {
-			st = new StringTokenizer(br.readLine());
-			for(int c= 0 ; c < M ; c++) {
-				office[r][c] = Integer.parseInt(st.nextToken());
-				if(office[r][c] > 0 && office[r][c] < 6) cctv.add(new int [] {office[r][c], r, c});
-			}
-		}
-        
-        //cctv가 한 대도 없을 때
-        if(cctv.size() == 0) {
-			int tmp = 0;
-			for(int r = 0 ; r < N ; r++) {
-				for(int c= 0 ; c < M ; c++) {
-					if(office[r][c] == 0) tmp++;
-				}
-			}
-			System.out.println(tmp);
-			return;
-		}
 
-		Collections.sort(cctv, new Comparator<int[]>(){
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o2[0] - o1[0];
-			}			
-		});
-		
-		//5번 cctv는 무조건 바꾸고 시작]
-		while(cctv.size() != 0 && cctv.get(0)[0] == 5) {
-			for(int dir = 0 ; dir < 4 ; dir++) {
-				change(dir, cctv.get(0)[1],cctv.get(0)[2]);
-			}
-			cctv.remove(0);
-		}
-		
-		answer = Integer.MAX_VALUE;
-		
-		BackTracking(0);
-		System.out.println(answer);
-	}
+    static int[] dr = {-1,0,1,0};
+    static int[] dc = {0,1,0,-1};
 
-	private static void BackTracking(int depth) {
-		if(depth == cctv.size()) {
-			//개수 세기
-			int tmp = 0;
-			for(int r = 0 ; r < N ; r++) {
-				for(int c= 0 ; c < M ; c++) {
-					if(office[r][c] == 0) tmp++;
-				}
-			}
-			if(answer > tmp) {
-				answer = tmp;
-			}
-			return;
-		}
-		
-		//재귀조건
-		for(int i = depth ; i < cctv.size() ; i++) {
-			//원상 복구를 위한 배열 만들기
-			int[][] restore = new int[N+1][M+1];
-			for(int r = 0 ; r < N ; r++) {
-				for(int c = 0 ; c < M ; c++) {
-					restore[r][c] = office[r][c];
-				}
-			}
-			
-			int[] cInfo = cctv.get(i);
-			if(cInfo[0] == 5) continue;
-			
-			switch(cInfo[0]) {
-				case 1 :
-					for(int dir = 0 ; dir < 4 ; dir++) {
-						change(dir, cInfo[1],cInfo[2]);
-						BackTracking(i+1);	
-						for(int r = 0 ; r < N ; r++) {
-							for(int c = 0 ; c < M ; c++) {
-								office[r][c] = restore[r][c];
-							}
-						}
-					}
-					break;
-					
-				case 2 :
-					for(int dir = 0 ; dir < 2 ; dir++) {
-						change(dir, cInfo[1],cInfo[2]);
-						change(dir+2, cInfo[1],cInfo[2]);	
-						BackTracking(i+1);	
-						for(int r = 0 ; r < N ; r++) {
-							for(int c = 0 ; c < M ; c++) {
-								office[r][c] = restore[r][c];
-							}
-						}
-					}
-					break;
-					
-				case 3 :
-					for(int dir = 0 ; dir < 4 ; dir++) {
-						change(dir, cInfo[1],cInfo[2]);
-						change((dir+1) % 4, cInfo[1],cInfo[2]);	
-						BackTracking(i+1);	
-						for(int r = 0 ; r < N ; r++) {
-							for(int c = 0 ; c < M ; c++) {
-								office[r][c] = restore[r][c];
-							}
-						}
-					}
-					break;
-					
-				case 4 :
-					for(int dir = 0 ; dir < 4 ; dir++) {
-						change(dir, cInfo[1],cInfo[2]);
-						change((dir+1) % 4, cInfo[1],cInfo[2]);	
-						change((dir+2) % 4, cInfo[1],cInfo[2]);	
-						BackTracking(i+1);	
-						for(int r = 0 ; r < N ; r++) {
-							for(int c = 0 ; c < M ; c++) {
-								office[r][c] = restore[r][c];
-							}
-						}
-					}
-					break;
-			}
-			
-			//원상복구하기
-			for(int r = 0 ; r < N ; r++) {
-				for(int c = 0 ; c < M ; c++) {
-					office[r][c] = restore[r][c];
-				}
-			}
-			
-		}		
-		
-	}
+    static int N, M, MIN;
 
-	private static void change(int d, int row, int col) {
-		int nr = row;
-		int nc = col;
-		//종료 조건 : 범위를 벗어났거나 벽을 마주했을 때
-		while(true) {
-			nr += dr[d];
-			nc += dc[d];
-			if(nr < 0 || nc < 0 || nr >= N || nc >= M) break;
-			if(office[nr][nc] == 6) break;
-			if(office[nr][nc] == 0) {
-				office[nr][nc] = -1;
-			}
-		}
-	}
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 1. 입력 받기, 전역 변수에 값 할당
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        int[][] office = new int[N][M]; // 사무실
+        ArrayList<int[]> cctvs = new ArrayList<>();
+        for(int r = 0 ; r < N ; r++) {
+            st = new StringTokenizer(br.readLine());
+            for(int c = 0 ; c < M ; c++) {
+                office[r][c] = Integer.parseInt(st.nextToken());
+
+                if(office[r][c] == 0 || office[r][c] == 6) continue;
+
+                if(office[r][c] == 5) cctvs.add(0, new int[]{5,r,c});
+                else cctvs.add(new int[]{office[r][c],r,c});
+            }
+        }
+
+        // 2. 5번 먼저 감시체계 돌리기
+        int cidx = 0;
+        while(cidx < cctvs.size() && cctvs.get(cidx)[0] == 5) {
+
+            int cr = cctvs.get(cidx)[1];
+            int cc = cctvs.get(cidx)[2];
+
+            for(int dir = 0 ; dir < 4 ; dir++) {
+                office = BigBrother(dir, office, cr,cc);
+            }
+            cidx++;
+        }
+
+        MIN = Integer.MAX_VALUE;
+        // 3. 나머지 감시체계 돌리기
+        if(cidx != cctvs.size()) BT(office, cidx, cctvs);
+        else MIN = count(office);
+
+        System.out.println(MIN);
+    }
+
+    public static void BT(int[][] office, int cIdx, ArrayList<int[]> cctvs) {
+        if(cIdx == cctvs.size()) {
+           int answer = count(office);
+
+            if(MIN > answer) MIN = answer;
+
+            return;
+        }
+
+            int cn = cctvs.get(cIdx)[0];
+            int cr = cctvs.get(cIdx)[1];
+            int cc = cctvs.get(cIdx)[2];
+
+            int[][] nOffice = copyOffice(office);
+
+            if(cn == 1) {
+                for(int j = 0 ; j < 4 ; j++) {
+                    nOffice = BigBrother(j, nOffice, cr, cc);
+                    BT(nOffice, cIdx+1, cctvs);
+                    nOffice = copyOffice(office);
+                }
+            } else if(cn == 2) {
+                for(int j = 0 ; j < 2 ; j++) {
+                    nOffice  = BigBrother(j, nOffice, cr,cc);
+                    nOffice  = BigBrother((j+2)%4, nOffice, cr,cc);
+                    BT(nOffice, cIdx+1, cctvs);
+                    nOffice = copyOffice(office);
+                }
+            } else if(cn == 3) {
+                for(int j = 0 ; j < 4 ; j++) {
+                    nOffice  = BigBrother(j, nOffice, cr,cc);
+                    nOffice  = BigBrother((j+1)%4, nOffice, cr,cc);
+                    BT(nOffice, cIdx+1, cctvs);
+                    nOffice = copyOffice(office);
+                }
+            } else if(cn == 4) {
+                for(int j = 0 ; j < 4 ; j++) {
+                    nOffice  = BigBrother(j, nOffice, cr,cc);
+                    nOffice = BigBrother((j+1)%4, nOffice, cr,cc);
+                    nOffice = BigBrother((j+2)%4, nOffice, cr,cc);
+                    BT(nOffice, cIdx+1, cctvs);
+                    nOffice = copyOffice(office);
+                }
+            }
+
+    }
+
+    public static int[][] BigBrother(int dir, int[][] nOffice, int cr , int cc){
+
+        while(true) {
+            int nr = cr + dr[dir];
+            int nc = cc + dc[dir];
+
+            // idx chk
+            if(nr < 0 || nc < 0 || nr >= N || nc >= M) break;
+
+            // wall chk
+            if(nOffice[nr][nc] == 6) break;
+
+            nOffice[nr][nc] = 7;
+            cr = nr;
+            cc = nc;
+        }
+
+        return nOffice;
+    }
+
+    public static int[][] copyOffice(int[][] office){
+        int[][] copy = new int[N][M];
+        for(int r = 0 ; r < N ; r++) {
+            for(int c = 0 ;c < M ; c++) {
+                copy[r][c] = office[r][c];
+            }
+        }
+
+        return copy;
+    }
+
+    public static int count(int[][] office) {
+        int answer = 0;
+        for(int r = 0 ; r < N ; r++) {
+            for(int c = 0 ; c < M ; c++) {
+                if(office[r][c] == 0) answer++;
+            }
+        }
+        return answer;
+    }
 }
