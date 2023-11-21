2ad441a3bc2a65dc1dedfe5c03b00df0de83d221
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2023-11-21 10:10:29 +0900

    [Silver II] Title: 에디터, Time: 648 ms, Memory: 137772 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,52 +1,68 @@
+// 09:54 -
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
+    // 1. 스택 2개 만들기 : 커서 기준 왼 오
+    public static ArrayDeque<Character> left, right;
+
+
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
+        String str = br.readLine();
+        int test = Integer.parseInt(br.readLine());
 
-        ArrayDeque<Character> stackLeft = new ArrayDeque<>();
-        ArrayDeque<Character> stackRight = new ArrayDeque<>();
-        for(int i = 0 ; i < input.length() ; i++) {
-            stackLeft.offerLast(input.charAt(i));
-        }
-        int M = Integer.parseInt(br.readLine());
+        left = new ArrayDeque<Character>();
+        right = new ArrayDeque<Character>();
 
-        int cursor = input.length(); // 커서 위치
-        for (int i = 0; i < M; i++) {
+        // 2. 초기 문자열 왼쪽 스택에 넣기
+        for(int i = 0 ; i < str.length() ; i++) left.offerLast(str.charAt(i));
+
+        // 3. 옮기기
+        for(int t = 0; t < test ; t++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             char command = st.nextToken().charAt(0);
 
-            switch (command) {
-                case 'L':
-                    if(!stackLeft.isEmpty()) {
-                        char ch = stackLeft.pollLast();
-                        stackRight.offerLast(ch);
-                    }
-                    break;
-                case 'D':
-                    if(!stackRight.isEmpty()) {
-                        char ch = stackRight.pollLast();
-                        stackLeft.offerLast(ch);
-                    }
-                    break;
-                case 'B':
-                    if(!stackLeft.isEmpty()) {
-                        stackLeft.pollLast();
-                    }
-                    break;
-                case 'P' :
-                    stackLeft.offerLast(st.nextToken().charAt(0));
-                    break;
-            }
+            if(command == 'L') L();
+            if(command == 'D') D();
+            if(command == 'B') B();
+            if(command == 'P') P(st.nextToken().charAt(0));
         }
 
-        while(!stackLeft.isEmpty()) sb.append(stackLeft.pollFirst());
-        while(!stackRight.isEmpty()) sb.append(stackRight.pollLast());
+        while(!left.isEmpty()) {
+            sb.append(left.pollFirst());
+        }
+        while(!right.isEmpty()) {
+            sb.append(right.pollLast());
+        }
 
         System.out.println(sb);
+    }
+
+    public static void L() {
+        if(left.isEmpty()) return;
+
+        char ch = left.pollLast();
+        right.offerLast(ch);
+    }
+
+    public static void D() {
+        if(right.isEmpty()) return;
+
+        char ch = right.pollLast();
+        left.offerLast(ch);
+    }
+
+    public static void B() {
+        if(left.isEmpty()) return;
+
+        left.pollLast();
+    }
+
+    public static void P(char insert) {
+        left.offerLast(insert);
     }
 }
