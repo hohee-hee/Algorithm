be588b453af04d20ea0749e9285f2d09cfe5ff3a
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-01-02 01:34:58 +0900

    [Bronze V] Title: 고양이, Time: 72 ms, Memory: 11220 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("\\    /\\").append('\n')
+                .append(" )  ( ')").append('\n')
+                .append("(  /  )").append('\n')
+                .append(" \\(__)|");
+
+        System.out.println(sb);
+    }
+}
