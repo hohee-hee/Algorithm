1140556d3277e699b4c98c0eec6844440043ed54
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-01-03 02:01:12 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 80 ms, Memory: 11616 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 1 ; i <= N ; i++) {
+            for(int j = i ; j < N ; j++) sb.append(" ");
+            for(int j = 1 ; j <= 2*i-1 ; j++) sb.append("*");
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
