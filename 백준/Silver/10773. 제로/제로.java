4ade08851e0f687fceba04f73b9b1b40f1ed51ac
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-01-05 17:18:13 +0900

    [Silver IV] Title: 제로, Time: 184 ms, Memory: 20184 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,43 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	static ArrayList<Integer> stack = new ArrayList<>();
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		int K = Integer.parseInt(br.readLine());
-		
-		//스택에 pop과 push하기
-		//0이면 pop , 0이 아니면 push
-		for(int i = 0 ; i < K ; i++) {
-			int num = Integer.parseInt(br.readLine());
-			if(num != 0) { push(num); }
-			else { pop(); }
-		}
-		
-		//합 구하기
-		int node_sum = 0;
-		while(stack.size() != 0) {
-			node_sum += pop();
-		}
-		
-		System.out.println(node_sum);
-	}
-	
-	//push
-	static void push(int x) {
-		stack.add(x);
-	}
-	
-	//pop
-	//합을 구할 때 사용하기 위하여 pop하는 노드의 원소 반환
-	static int pop() {
-		int ele = stack.get(stack.size() - 1);
-		stack.remove(stack.size() - 1);
-		return ele;
-	}
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        for(int i = 0 ; i < K ; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0) stack.pollFirst();
+            else stack.offerFirst(num);
+        }
+
+        int answer = 0;
+        while(!stack.isEmpty()) answer += stack.pollFirst();
+
+        System.out.println(answer);
+    }
 }
