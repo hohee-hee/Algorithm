a14ac2394a9d20edb9f96c009e340ab3933adf51
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-01-08 22:13:30 +0900

    [Gold V] Title: AC, Time: 552 ms, Memory: 85836 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -2,64 +2,61 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder(); 
-        
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        
-        outer: for(int tc = 0 ; tc < T ; tc++) {
-        	// 1. 입력 받기
-        	String command = br.readLine();
-        	int N = Integer.parseInt(br.readLine());
-        	String list = br.readLine();
-        	
-        	String[] nums  = list.substring(1, list.length()-1).split(",");        	
-        	
-        	// 2. 입력받은 숫자를 덱에 넣기
-        	ArrayDeque<Integer> dq = new ArrayDeque<>();
-        	for(int i = 0 ; i < N ; i++){
-        		dq.offerLast(Integer.parseInt(nums[i]));
-        	}
-        	
-        	boolean dir = true; // true : pollFirst, false: pollLast
-        	
-        	// 3. 명령어 수행하기
-        	for(int i = 0 ; i < command.length() ; i++) {
-        		if(command.charAt(i) == 'R') {
-        			dir = !dir;
-        		} else if (dq.isEmpty()) { 
-        			sb.append("error\n");
-        			continue outer;
-        		} else if (dir) {
-        			dq.pollFirst();
-        		} else {
-        			dq.pollLast();
-        		}
-        	}
-        	
-        	int[] ac = new int[dq.size()];
-        	if(!dir) {
-        		for(int i = 0 ; i < ac.length ; i++) {
-        			ac[i] = dq.pollLast();
-        		}
-        	} else {
-        		for(int i = 0 ; i < ac.length ; i++) {
-        			ac[i] = dq.pollFirst();
-        		}        		
-        	}
-        	
-        	sb.append("[");
-        	for(int i = 0 ; i < ac.length ; i ++) {
-        		sb.append(ac[i]);
-        		if(i != ac.length - 1) {
-        			sb.append(",");
-        		}
-        	}
-        	sb.append("]\n");
-        	
+
+        for(int tc = 0 ; tc < T ; tc++) {
+            String p = br.readLine();
+            int n = Integer.parseInt(br.readLine());
+            String array = br.readLine();
+            array = array.substring(1, array.length()-1);
+
+            ArrayDeque<String> dq = new ArrayDeque<>();
+            if(array.length() != 0) {
+                String[] nums =  array.split(",");
+                for(int i = 0 ; i < nums.length ; i++) dq.offerLast(nums[i]);
+            }
+
+            boolean isAsc = true;
+            boolean isError = false;
+            for(int i = 0 ; i < p.length() ; i++) {
+                char command = p.charAt(i);
+
+                if(command == 'R') {
+                    isAsc = !isAsc;
+                } else {
+                    if(dq.isEmpty()) {
+                        isError = true;
+                        break;
+                    }
+
+                    if(isAsc) dq.pollFirst();
+                    else dq.pollLast();
+                }
+            }
+
+            if(isError) {
+                sb.append("error").append("\n");
+            } else if(dq.isEmpty()) {
+              sb.append("[]").append("\n");
+            } else {
+                sb.append("[");
+                if(isAsc) {
+                while(!dq.isEmpty()) sb.append(dq.pollFirst()).append(",");
+                sb.deleteCharAt(sb.length() - 1);
+                } else {
+                while(!dq.isEmpty()) sb.append(dq.pollLast()).append(",");
+                sb.deleteCharAt(sb.length() - 1);
+                }
+
+                sb.append("]").append("\n");
+            }
         }
-        
-		System.out.println(sb);
-	}
+
+
+        System.out.println(sb);
+    }
 }
