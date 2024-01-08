a484b5bf75cc83f2cc13f68389ef84bdc333d1d2
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-01-08 19:37:40 +0900

    [Silver IV] Title: 덱, Time: 144 ms, Memory: 16412 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -1,90 +1,92 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static LinkedList<Integer> dq = new LinkedList<>();
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		for(int i = 0 ; i < N ; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			switch(st.nextToken()) {
-			case "push_front":
-				push_front(Integer.parseInt(st.nextToken()));
-				break;
-			case "push_back":
-				push_back(Integer.parseInt(st.nextToken()));
-				break;
-			case "pop_front":
-				pop_front();
-				break;
-			case "pop_back":
-				pop_back();
-				break;
-			case "size":
-				size();
-				break;
-			case "empty":
-				isEmpty();
-				break;
-			case "front":
-				front();
-				break;
-			case "back":
-				back();
-				break;
-			}
-		}
-		System.out.println(sb);
-	}
-	
-	static void push_front(int x) {
-		dq.add(0, x);
-	}
-	
-	static void push_back(int x) {
-		dq.add(dq.size(), x);
-	}
-	
-	static void pop_front() {
-		if(dq.size() == 0) sb.append(-1).append("\n");
-		else {
-			sb.append(dq.get(0)).append("\n");
-			dq.remove(0);
-		}
-	}
-	
-	static void pop_back() {
-		if(dq.size() == 0) sb.append(-1).append("\n");
-		else {
-			sb.append(dq.get(dq.size()-1)).append("\n");
-			dq.remove(dq.size()-1);
-		}
-	}
-	
-	static void size() {
-		sb.append(dq.size()).append("\n");
-	}
 
-	static void isEmpty() {
-		if(dq.size() == 0) sb.append(1).append("\n");
-		else sb.append(0).append("\n");
-	}
-	
-	static void front() {
-		if(dq.size() == 0) sb.append(-1).append("\n");
-		else sb.append(dq.get(0)).append("\n");
-	}
-	
-	static void back() {
-		if(dq.size() == 0) sb.append(-1).append("\n");
-		else sb.append(dq.get(dq.size()-1)).append("\n");
-	}
+    public static int[] DEQUE = new int[20002];
+    public static int FRONT = 10000;
+    public static int BACK = 10001;
+
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int tc = 0 ; tc < N ; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+
+            switch(command) {
+                case "push_front":
+                    push_front(Integer.parseInt(st.nextToken()));
+                    break;
+                case "push_back":
+                    push_back(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop_front":
+                    sb.append(pop_front()).append("\n");
+                    break;
+                case "pop_back":
+                    sb.append(pop_back()).append("\n");
+                    break;
+                case "size":
+                    sb.append(size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(empty()).append("\n");
+                    break;
+                case "front":
+                    sb.append(front()).append("\n");
+                    break;
+                case "back":
+                    sb.append(back()).append("\n");
+                    break;
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void push_front(int x) {
+        DEQUE[FRONT] = x;
+        FRONT--;
+    }
+
+    public static void push_back(int x) {
+        DEQUE[BACK] = x;
+        BACK++;
+    }
+
+    public static int pop_front(){
+        if(FRONT == (BACK - 1)) return -1;
+        return DEQUE[++FRONT];
+    }
+
+    public static int pop_back(){
+        if(FRONT == (BACK - 1)) return -1;
+        return DEQUE[--BACK];
+    }
+
+    public static int size() {
+        if(FRONT == (BACK - 1)) return 0;
+        return BACK - FRONT - 1;
+    }
+
+    public static int empty() {
+        if(FRONT == (BACK - 1)) return 1;
+        else return 0;
+    }
+
+    public static int front() {
+        if(FRONT == (BACK - 1)) return -1;
+        return DEQUE[FRONT+1];
+    }
+
+    public static int back() {
+        if(FRONT == (BACK - 1)) return -1;
+        return DEQUE[BACK-1];
+    }
+
+
 }
-
-
