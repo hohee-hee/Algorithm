ca980410c228c63fb6a7f522fe77cd3f2593f54d
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-01-10 00:07:15 +0900

    [Silver II] Title: 쇠막대기, Time: 116 ms, Memory: 14556 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,32 +1,32 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		//스택선언
-		Stack<Character> s = new Stack<>();
-		
-		//입력받기
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-		char[] str = br.readLine().toCharArray();
-		
-		int piece = 0 ;//막대기 조각
-		
-		for(int i = 0 ; i < str.length ; i++) {
-			if(str[i] == '(') { s.push(str[i]); }
-			else {
-				if(str[i-1] == '(') { 
-					s.pop();
-					piece += s.size();
-				}
-				else {
-					s.pop();
-					piece++;
-				}
-			}			
-		}
-		
-		System.out.println(piece);
-		
-	}
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        int answer = 0;
+
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        for(int i = 0 ; i < str.length() ; i++) {
+            char ch = str.charAt(i);
+
+            if(ch == '(') {
+                if(i != str.length() - 1 && str.charAt(i+1) == ')') {
+                    answer += stack.size();
+                    i++;
+                } else {
+                    stack.offerFirst(ch);
+                }
+            } else {
+                answer++;
+                stack.pollFirst();
+            }
+        }
+
+
+        System.out.println(answer);
+    }
 }
+
