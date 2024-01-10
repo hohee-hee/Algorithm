e4e37acfb2170c9e25bd65b88fd4cd0355996853
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-01-11 02:55:51 +0900

    [Gold V] Title: 토마토, Time: 664 ms, Memory: 110308 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,83 +1,75 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-		
-	public static void main(String[] args)  throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int C = Integer.parseInt(st.nextToken());
-		int R = Integer.parseInt(st.nextToken());
-		int H = Integer.parseInt(st.nextToken());
-		int[][][] tomato = new int[H][R][C];
-		int[][][] dist = new int[H][R][C];
-		Deque<int[]> q = new ArrayDeque<>();
-		boolean isRipen = true;
-		//입력 받기
-		for(int i = 0 ; i < H ; i++) {
-			for(int j = 0 ; j < R ; j++) {
-				st = new StringTokenizer(br.readLine());
-				for(int k = 0 ; k < C ; k++) {
-					tomato[i][j][k] = Integer.parseInt(st.nextToken());
-					if(tomato[i][j][k] == 1) {
-						int[] ripe = {i, j, k};
-						q.offerLast(ripe);
-					}
-					else if(tomato[i][j][k] == 0) {
-						dist[i][j][k] = -1;
-						isRipen = false;
-					}
-				}
-			}
-		}
-		
-		//만약 이미 모두 익어있다면 0 출력
-		if(isRipen) {
-			System.out.println(0);
-			return;
-		}
-		
-		//델타배열 선언
-		int[][] delta = {{1, 0, 0}, {-1, 0, 0}, {0, 0, -1}, {0, 0, 1}, {0, -1, 0}, {0, 1, 0}};
-		
-		//BFS
-		
-		while(!q.isEmpty()) {
-			int[] pt = q.pollFirst();
-			int ch = pt[0];
-			int cr = pt[1];
-			int cc = pt[2];
-			for(int i = 0 ; i < 6 ; i++) {
-				int nh = ch + delta[i][0];
-				int nr = cr + delta[i][1];
-				int nc = cc + delta[i][2];
+public class Main {
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
 
-				if(nh < 0 || nr < 0 || nc < 0 || nh >= H || nr >= R || nc >= C || dist[nh][nr][nc] >= 0)
-					continue;
-				dist[nh][nr][nc] = dist[ch][cr][cc] + 1;
-				tomato[nh][nr][nc] = 1;
-				int[] ripe = {nh, nr, nc};
-				q.offerLast(ripe);
-			}
-		}		
-		
-		
-		//출력
-		int max = -1;
-		for(int i = 0 ; i < H ; i++) {
-			for(int j = 0 ; j < R ; j++) {
-				for(int k = 0 ; k < C ; k++) {
-					if(dist[i][j][k] == 0) continue;
-					if(dist[i][j][k] == -1) {
-						System.out.println(-1);
-						return;
-					}
-					if(dist[i][j][k] > max) {
-						max = dist[i][j][k];
-					}
-				}
-			}
-		}
-		System.out.println(max);
-	}	
+        // `1. 입력 받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+
+        int[][][] boxes = new int[H][N][M];
+        ArrayDeque<int[]> tomatoes = new ArrayDeque<>();
+        for(int h = 0 ; h < H ; h++) {
+            for(int r = 0 ; r < N ; r++) {
+                st = new StringTokenizer(br.readLine());
+                for(int c = 0 ; c < M ; c++) {
+                    boxes[h][r][c] = Integer.parseInt(st.nextToken());
+
+                    if(boxes[h][r][c] == 1) tomatoes.offerLast(new int[]{h,r,c});
+                }
+            }
+        }
+
+        // 2. bfs
+        int[] dh = {0,0,0,0,-1,1};
+        int[] dr = {-1,0,1,0,0,0};
+        int[] dc = {0,1,0,-1,0,0};
+        int[][][] dist = new int[H][N][M];
+        while(!tomatoes.isEmpty()) {
+            int ch = tomatoes.peekFirst()[0];
+            int cr = tomatoes.peekFirst()[1];
+            int cc = tomatoes.pollFirst()[2];
+            int cd = dist[ch][cr][cc];
+
+            for(int dir = 0 ; dir < 6 ; dir++) {
+                int nh = ch + dh[dir];
+                int nr = cr + dr[dir];
+                int nc = cc + dc[dir];
+                int nd = cd + 1;
+
+                if(nh < 0 || nh >= H || nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+                if(boxes[nh][nr][nc] != 0) continue;
+                if(dist[nh][nr][nc] != 0 && dist[nh][nr][nc] < nd) continue;
+
+                dist[nh][nr][nc] = nd;
+                boxes[nh][nr][nc] = 1;
+                tomatoes.offerLast(new int[]{nh,nr,nc});
+            }
+        }
+
+        // 3. 답 찾기
+        int answer = 0;
+        outer: for(int h = 0 ; h < H ; h++) {
+            for(int r = 0 ; r < N ; r++) {
+                for(int c = 0 ; c < M ; c++) {
+                    if(boxes[h][r][c] == -1) continue;
+
+                    if(boxes[h][r][c] == 0) {
+                        answer = -1;
+                        break outer;
+                    }
+
+                    if(answer < dist[h][r][c]) answer = dist[h][r][c];
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
 }
