fcf4a0d6ac712eee61161e86f49df61f67dfdcb8
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-01-12 02:55:57 +0900

    [Gold III] Title: 다리 만들기, Time: 204 ms, Memory: 76968 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -2,178 +2,100 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	static int[][] map; //섬 정보 저장
-	static int N; //map의 크기
-	
-	static int[][] dist; //거리 정보저장
-	static boolean[][] isVisited; //방문 여부 체크
-	
-	public static void main(String[] args) throws IOException {
-		//1. 입력 및 초기화
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		//- 지도 정보 입력 받기
-		map = new int[N][N];
-		for(int r = 0 ; r < N ; r++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int c = 0 ; c < N ; c++) {
-				map[r][c] = Integer.parseInt(st.nextToken());
-			}
-		}
-		//- 변수 초기화
-		isVisited = new boolean[N][N];
-		
-		//2. 델타배열 선언
-		int[] dr = {0,-1,0,1};
-		int[] dc = {-1,0,1,0};
-		
-		
-		//3. 각 섬에 넘버링 하기  : BFS
-		int num = 1;
-		for(int r = 0 ; r < N ; r++) {
-			for(int c = 0 ; c < N ; c++) {
-				//-1. 시작점 찾기
-				//- 바다인 경우
-				if(map[r][c] == 0) continue;
-				//- 이미 방문한 경우
-				if(isVisited[r][c]) continue;
-				
-				//- 시작점 찾았다!
-				ArrayDeque<int[]> q = new ArrayDeque<>();
-				q.offerLast(new int[] {r,c});
-				map[r][c] = num;
-				
-				//2. BFS				
-				while(!q.isEmpty()) {
-					int cr = q.peekFirst()[0];
-					int cc = q.pollFirst()[1];
-					for(int dir = 0 ; dir < 4 ; dir++) {
-						int nr = cr + dr[dir];
-						int nc = cc + dc[dir];
-						
-						//out of Bound
-						if(nr < 0 || nr >= N || nc <0 || nc >= N) continue;
-						//바다 일때
-						if(map[nr][nc] == 0) continue;
-						//이미 방문했을 때
-						if(isVisited[nr][nc]) continue;
-						
-						//방문체크
-						isVisited[nr][nc] = true;
-						
-						//넘버링
-						map[nr][nc] = num;
-						
-						//enqueue
-						q.offerLast(new int[] {nr,nc});
-					}			
-				}
-				
-				//3. 섬 넘버링 더하기
-				num++;
-			}
-		}		
-		
-		//3. 거리구하기
-		int dmin = Integer.MAX_VALUE; //최소거리를 저장
-		int[] minDist = new int[num-2]; //최소 거리를 저장할 배열
-		
-		for(int r = 0 ; r < N ; r++) {
-			for(int c = 0 ; c < N ; c++) {
-				//-1. 시작점 찾기
-				//- 바다인 경우
-				if(map[r][c] == 0) continue;
-				
-				//- 시작점 찾았다!
-				ArrayDeque<int[]> q = new ArrayDeque<>();
-				q.offerLast(new int[] {r,c});
-				num = map[r][c];
-				
-				//- 방문 여부 확인 변수 초기화
-				isVisited = new boolean[N][N];
-				
-				//- 거리 저장할 배열 초기화
-				dist = new int[N][N];				
-				
-				//2. BFS				
-				while(!q.isEmpty()) {
-					int cr = q.peekFirst()[0];
-					int cc = q.pollFirst()[1];
-					for(int dir = 0 ; dir < 4 ; dir++) {
-						int nr = cr + dr[dir];
-						int nc = cc + dc[dir];
-						
-						//out of Bound
-						if(nr < 0 || nr >= N || nc <0 || nc >= N) continue;
-						//바다가 아닐 때
-						if(map[nr][nc] != 0) continue;
-						//이미 방문했을 때
-						if(isVisited[nr][nc]) continue;
-						
-						//방문체크
-						isVisited[nr][nc] = true;
-						
-						//거리 추가
-						dist[nr][nc] = dist[cr][cc] + 1;
-						
-						//enqueue
-						q.offerLast(new int[] {nr,nc});
-					}			
-				}
-				
-				
-				//3. 최소 거리 찾기
-				int tmp = findDist(num);
-				if(dmin > tmp) dmin = tmp;
-				
-				//4. 조기종료 조건 : 최소값이 1이면 더 이상 작아질 수 없음
-				if(dmin == 1) {
-					System.out.println(dmin);
-					return;
-				}	
-				
-			}
-		}
-		
-		//출력
-		System.out.println(dmin);		
-	}	
-	
-	private static int findDist(int num) {
-		//1. 반환할 변수
-		int re = Integer.MAX_VALUE;
-		
-		//2. 델타배열 선언
-		int[] dr = {0,-1,0,1};
-		int[] dc = {-1,0,1,0};
-		
-		//최소 거리 찾기
-		for(int r = 0 ; r < N ; r++) {
-			for(int c = 0 ; c < N ; c++) {
-				//바다거나 자기 자신이거나 이미 거리를 계산한 적이 있는 섬이면 pass
-				if(map[r][c] <= num) continue;
-				
-				//그렇지 않으면 주변 4방향의 거리 보기
-				for(int dir = 0 ; dir < 4 ; dir++) {
-					int nr = r + dr[dir];
-					int nc = c + dc[dir];
-					
-					//out of Bound
-					if(nr < 0 || nr >= N || nc <0 || nc >= N) continue;
-					
-					int ndist = dist[nr][nc];
-					
-					//주변거리가 0이면 섬이라는 의미이므로 pass
-					if(ndist == 0) continue;
-				
-					//그렇지 않으면 re랑 비교해서 저장해주기
-					if(re > ndist) re = ndist;
-					//- 조기종료 조건 : 최소값이 1이면 더 이상 작아질 수 없음
-					if(re == 1) return re;
-				}
-			}
-		}		
-		return re;
-	}
+    public static void main(String[] args) throws IOException{
+        // 1. 입력받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] map = new int[N][N];
+        for(int r = 0 ; r < N ; r++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int c = 0 ; c < N ; c++) {
+                map[r][c] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 2. 섬 넘버링 하면서 가장자리 큐에 넣기
+        int[] dr = {-1,0,1,0};
+        int[] dc = {0,1,0,-1};
+        int iNum = 1;
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        ArrayDeque<int[]> edge = new ArrayDeque<>();
+        boolean[][] isVisited = new boolean[N][N];
+        for(int r = 0 ; r < N ; r++) {
+            for(int c = 0 ; c < N ; c++) {
+                if(map[r][c] == 0) continue;
+                if(isVisited[r][c]) continue;
+
+                boolean isEdge = false;
+                q.offerLast(new int[]{r,c});
+                isVisited[r][c] = true;
+                map[r][c] = iNum;
+                while(!q.isEmpty()) {
+                    int cr = q.peekFirst()[0];
+                    int cc = q.pollFirst()[1];
+
+                    for(int dir = 0 ; dir < 4 ; dir++) {
+                        int nr = cr + dr[dir];
+                        int nc = cc + dc[dir];
+
+                        if(nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+                        if(isVisited[nr][nc]) continue;
+                        if(map[nr][nc] == 0) {
+                            isEdge = true;
+                            continue;
+                        }
+
+                        q.offerLast(new int[]{nr, nc});
+                        isVisited[nr][nc] = true;
+                        map[nr][nc] = iNum;
+                    }
+
+                    if(isEdge) edge.offerLast(new int[]{cr,cc});
+                    isEdge = false;
+                }
+                iNum++;
+            }
+        }
+
+        // 3. 다리 잇기
+        int answer = Integer.MAX_VALUE;
+        while(!edge.isEmpty()) {
+            int r = edge.peekFirst()[0];
+            int c = edge.pollFirst()[1];
+
+            isVisited = new boolean[N][N];
+            q = new ArrayDeque<>();
+            q.offerLast(new int[]{r,c,0});
+            isVisited[r][c] = true;
+
+            bfs: while(!q.isEmpty()) {
+                int cr = q.peekFirst()[0];
+                int cc = q.peekFirst()[1];
+                int cd = q.pollFirst()[2];
+
+                for(int dir = 0 ; dir < 4 ; dir++) {
+                    int nr = cr + dr[dir];
+                    int nc = cc + dc[dir];
+                    int nd = cd + 1;
+
+                    if(nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+                    if(map[nr][nc] == map[r][c]) continue;
+                    if(isVisited[nr][nc]) continue;
+
+                    if(map[nr][nc] > 0 && map[nr][nc] != map[r][c]) {
+                        if(answer > cd) answer = cd;
+                        break bfs;
+                    }
+
+                    q.offerLast(new int[]{nr, nc, nd});
+                    isVisited[nr][nc] = true;
+                }
+            }
+        }
+
+
+        // 3. 최단거리 구하기
+        System.out.println(answer);
+    }
 }
