512dcd695ffbeb7ebc210b786aff9a5daee0d642
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-01-16 15:11:27 +0900

    [Silver II] Title: 로또, Time: 80 ms, Memory: 11656 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,62 +1,40 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int K;
-	static int[] S;
-	static int[] per;
-	static boolean[] isUsed;
-	static StringBuilder sb;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		sb = new StringBuilder();
-		StringTokenizer st;
-		// 반복
-		while(true) {
-			st = new StringTokenizer(br.readLine());
-			K = Integer.parseInt(st.nextToken());
-			//종료조건
-			if(K == 0) break;
-			
-			//필요한 배열 초기화
-			S = new int[K]; //입력받을 집합 S
-			per = new int[6]; //가능한 번호 배열을 저장할 배열
-			isUsed = new boolean[K]; //사용 여부 확인
-			
-			//입력받기
-			for(int i = 0 ; i < K ; i++) {
-				S[i] = Integer.parseInt(st.nextToken());
-			}
-			
-			//백트래킹
-			permunation(0);
-			
-			sb.append("\n");
-		}
-		
-		//출력
-		System.out.println(sb);
-	}
+    public static StringBuilder sb;
+    public static void main(String[] args) throws IOException{
+        sb = new StringBuilder();
 
-	private static void permunation(int idx) {
-		//기저조건
-		if(idx == 6) {
-			for(int i = 0 ; i < 6 ; i++) {
-				sb.append(per[i] + " ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		for(int i = 0 ; i < K ; i++) {
-			if(idx == 0 && i > K-6) {
-				return;
-			}
-			if(idx==0 || per[idx-1] < S[i]) {
-				per[idx] = S[i];
-				permunation(idx+1);
-			}
-		}
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int k = Integer.parseInt(st.nextToken());
+            if(k == 0) break;
+
+            int[] S = new int[k];
+            for(int i = 0 ; i < k ; i++) S[i] = Integer.parseInt(st.nextToken());
+
+            bt(0, 0, k, S, new int[6]);
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void bt (int cnt, int sidx, int k, int[] S, int[] lotto) {
+        if(cnt == 6) {
+            for(int i = 0 ; i < 6 ; i++) sb.append(lotto[i]).append(" ");
+            sb.append("\n");
+
+            return;
+        }
+
+        for(int i = sidx ; i < k ; i++) {
+            lotto[cnt] = S[i];
+            bt(cnt+1, i+1, k, S, lotto);
+        }
+    }
 }
