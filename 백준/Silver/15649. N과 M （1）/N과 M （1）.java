92934303235d2ae7e22dd7df635bceaa32541e49
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-01-16 13:00:35 +0900

    [Silver III] Title: N과 M (1), Time: 184 ms, Memory: 20612 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,45 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main{
-	public static int[] arr;
-	public static boolean[] visit;
-	
+public class Main {
+    public static StringBuilder sb;
+    public static int[] per;
+    public static boolean[] isChosen;
+
     public static void main(String[] args) throws IOException{
-    	//입력 받기
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		arr = new int[M];
-		visit = new boolean[N];
-		
-		dfs(N, M, 0);
+        sb = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        per = new int[M];
+        isChosen = new boolean[N+1];
+
+        bt(0, N, M);
+
+        System.out.println(sb);
     }
-    
-    public static void dfs(int N, int M, int depth){
-    	if(M == depth) {
-    		StringBuilder sb = new StringBuilder();
-    		for(int i = 0 ; i < M ; i++) {
-    			sb.append(arr[i]).append(" ");
-    		}
-    		System.out.println(sb);
-    		return;
-    	}
-    	
-    	for(int i = 0 ; i < N ; i++) {
-    		if(!visit[i]) {
-    			visit[i] = true;
-    			arr[depth] = i+1;
-    			dfs(N,M,depth+1);
-    			visit[i] = false;
-    		}
-    	}
-    	
-    	
-    	return;
+
+    public static void bt(int cnt, int N, int M) {
+        if(cnt == M) {
+            for(int i = 0; i < M ; i++) sb.append(per[i]).append(" ");
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 1; i <= N ; i++) {
+            if(isChosen[i]) continue;
+            per[cnt] = i;
+            isChosen[i] = true;
+            bt(cnt+1, N, M);
+            isChosen[i] = false;
+            per[cnt] = 0;
+        }
+
     }
 }
