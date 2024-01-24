3b5c36584fb507eaac685a7cd94c591adeec080c
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-01-24 16:58:47 +0900

    [Gold V] Title: 톱니바퀴, Time: 80 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -1,141 +1,74 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-	
-	static ArrayList<Integer>[] wheels; //톱니바퀴 정보
-	static ArrayList<int[]> toMove; //움직여야하는 톱니바퀴 정보
-	
-	public static void main(String[] args) throws IOException {
-		//1. 입력 받기
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		//- 톱니바퀴 정보 입력
-		wheels = new ArrayList[4];
-		for(int i = 0 ; i < 4 ; i++) wheels[i] = new ArrayList<>();		
-		for(int i = 0 ; i < 4 ; i++) {
-			String str = br.readLine();
-			for(int j = 0 ; j < 8 ; j++) {
-				wheels[i].add(str.charAt(j) - '0');
-			}
-		}
-		
-		//- K
-		int K = Integer.parseInt(br.readLine());
-		
-		//- 회전 정보 저장
-		int[][] turn = new int[K][2];
-		for(int i = 0 ; i < K ; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			turn[i][0] = Integer.parseInt(st.nextToken());
-			turn[i][1] = Integer.parseInt(st.nextToken());
-		}
-		
-		//2. 회전하기
-		for(int i = 0 ; i < K ; i++) {
-			int wnum = turn[i][0]-1; //톱니바퀴 번호
-			int dir = turn[i][1]; //회전 방향
-			
-			//1. 회전하는 바퀴 찾기
-			//회전하는 바퀴 정보를 저장할 배열 선언
-			toMove = new ArrayList<>();
-			//현재 바퀴 정보 저장
-			toMove.add(new int[] {wnum, dir});
-			//움직여야하는 바퀴 정보 저장하기
-			find(wnum, dir);
-			
-			//2. 바퀴 움직이기
-			for(int j = 0 ; j < toMove.size() ; j++) {
-				int num = toMove.get(j)[0];
-				int d = toMove.get(j)[1];
-				
-				//시계방향 회전
-				if(d == 1) {
-					//맨 뒤 애를 빼와서
-					int remove = wheels[num].remove(7);
-					//맨 앞에 추가
-					wheels[num].add(0, remove);
-				}
-				
-				//반시계 회전
-				else {
-					//맨 앞 애를 빼 와서
-					int remove = wheels[num].remove(0);
-					//맨 뒤에 추가
-					wheels[num].add(remove);					
-				}
-			}
-		}
-		
-		//3. 점수 계산하기
-		int score = 0;
-		for(int i = 0 ; i < 4; i++) {
-			if(wheels[i].get(0) == 1) score += (int) Math.pow(2, i);
-		}
-		
-		//4. 출력하기
-		System.out.println(score);
-	}
+    public static int[][] wheels;
+    public static int[] pointer;
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
 
-	private static void find(int wnum, int dir) {
-		switch (wnum){
-			case 0 :
-				//우측에 있는 모든 바퀴 검색
-				//현재 바퀴와 맞닿은 톱니의 극이 같다면 멈추기
-				for(int i = wnum ; i < 3 ; i++) {
-					//맞닿은 톱니의 극이 같음
-					if(wheels[i].get(2) == wheels[i+1].get(6)) break;
-					
-					//같지 않음
-					dir *= -1;
-					toMove.add(new int[] {i+1,dir});					
-				}
-				
-				break;
-			case 1 :
-				//왼쪽 바퀴(0번 바퀴)부터 검사
-				if(wheels[wnum-1].get(2) != wheels[wnum].get(6)) {
-					toMove.add(new int[] {wnum-1,dir*(-1)});
-				}
-				
-				//오른쪽 바퀴들 검사
-				for(int i = wnum ; i < 3 ; i++) {
-					//맞닿은 톱니의 극이 같음
-					if(wheels[i].get(2) == wheels[i+1].get(6)) break;
-					
-					//같지 않음
-					dir *= -1;
-					toMove.add(new int[] {i+1,dir});
-				}
-				
-				break;
-			case 2 :
-				//오른쪽 바퀴(3번바퀴)부터 검사
-				if(wheels[wnum].get(2) != wheels[wnum+1].get(6)) toMove.add(new int[] {wnum+1,dir*(-1)});
-				//왼쪽 바퀴들 검사
-				for(int i = wnum ; i > 0 ; i--) {
-					//맞닿은 톱니의 극이 같음
-					if(wheels[i-1].get(2) == wheels[i].get(6)) break;
-					
-					//같지 않음
-					dir *= -1;
-					toMove.add(new int[] {i-1,dir});					
-				}
-				
-				break;
-			case 3:
-				//좌측에 있는 모든 바퀴 탐색
-				for(int i = wnum; i > 0 ;i--) {
-					//맞닿은 톱니의 극이 같음
-					if(wheels[i-1].get(2) == wheels[i].get(6)) break;
-					
-					//같지 않음
-					dir *= -1;
-					toMove.add(new int[] {i-1,dir});
-				}
-				
-				break;
-		}
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        wheels = new int[4][8];
+        pointer = new int[4];
+        for(int i = 0 ; i < 4 ; i++) {
+            String str = br.readLine();
+            for(int j = 0 ; j < 8 ; j++) {
+                wheels[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        int K = Integer.parseInt(br.readLine());
+
+        for(int i = 0 ; i < K ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken()) - 1;
+            int dir = Integer.parseInt(st.nextToken());
+
+            int[] newpointer = turnWheel(num, dir);
+
+            newpointer[num] = dir;
+
+            for(int p = 0 ; p < 4 ; p++) {
+                pointer[p] += (newpointer[p] * (-1));
+                if(pointer[p] == -1) pointer[p] = 7;
+                else if(pointer[p] == 8) pointer[p] = 0;
+            }
+        }
+
+        int answer = 0;
+        answer += wheels[0][pointer[0]] * 1;
+        answer += wheels[1][pointer[1]] * 2;
+        answer += wheels[2][pointer[2]] * 4;
+        answer += wheels[3][pointer[3]] * 8;
+
+        System.out.println(answer);
+    }
+
+    public static int[] turnWheel(int wheelNum , int turnDir){
+        int[] newpointer = new int[4];
+
+        // 왼쪽 돌리기
+        int lwheel = wheelNum - 1;
+        int prevTurn = turnDir;
+        while(lwheel >= 0) {
+            if(wheels[lwheel+1][(pointer[lwheel+1] + 6) % 8] == wheels[lwheel][(pointer[lwheel] + 2) % 8]) break;
+
+            newpointer[lwheel] = prevTurn * (-1);
+            prevTurn *= (-1);
+            lwheel--;
+        }
+
+        // 오른쪽 돌리기
+        int rwheel = wheelNum + 1;
+        prevTurn = turnDir;
+        while(rwheel < 4) {
+            if(wheels[rwheel-1][(pointer[rwheel-1] + 2) % 8] == wheels[rwheel][(pointer[rwheel] + 6) % 8]) break;
+
+            newpointer[rwheel] = prevTurn * (-1);
+            prevTurn *= (-1);
+            rwheel++;
+        }
+
+        return newpointer;
+    }
 }
