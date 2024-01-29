55f84f9de1e41fc95fdadc13fac495aea85138bb
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-01-29 22:07:07 +0900

    [Silver V] Title: 단어 정렬, Time: 260 ms, Memory: 28200 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,31 +1,26 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		String[] words = new String[N];
-		for(int i = 0 ; i < N ; i++) {
-			words[i] = sc.next();
-		}
-		
-		Arrays.sort(words,new Comparator<String>() {
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
 
-			@Override
-			public int compare(String o1, String o2) {
-				if(o1.length() == o2.length())
-					return o1.compareTo(o2);
-				else
-					return o1.length() - o2.length();
-			}
-			
-		});
-		for(int i = 0 ; i < N ; i++) {
-			if(i == 0)
-				System.out.println(words[i]);
-			else if(!words[i].equals(words[i-1]))
-				System.out.println(words[i]);
-			
-		}
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        TreeSet<String> words = new TreeSet<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length()) return o1.compareTo(o2);
+                else return o1.length() - o2.length();
+            }
+        });
+
+        for(int i = 0 ; i < N ; i++) words.add(br.readLine());
+
+        Iterator iter = words.iterator();
+        while(iter.hasNext()) sb.append(iter.next()).append("\n");
+
+        System.out.println(sb);
+    }
 }
+
