6ef531cdb7b299f660a96bd17e3b219b04683f0e
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-01-31 00:52:03 +0900

    [Silver I] Title: 정수 삼각형, Time: 224 ms, Memory: 23596 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,41 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
 
-		// 1. 입력 받기 
-		int N = Integer.parseInt(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] tri = new int[n+1][n+1];
+        for(int i = 1 ; i <= n ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 1 ; j <= i ; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                tri[i][j] = Math.max(tri[i-1][j-1], tri[i-1][j]) + num;
+            }
+        }
 
-		int[][] tri = new int[N+1][N+1]; // 삼각형 정보 저장
-		int[][] DP = new int[N+1][N+1]; // DP 배열
-		
-		for(int r = 1 ; r <= N ; r++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int c = 1 ; c <= r ; c++) {
-				tri[r][c] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		// 2. DP		
-		for(int r = 1 ; r <= N ; r++) {
-			for(int c = 1 ; c <= r ; c++) {
-				DP[r][c] = Math.max(DP[r-1][c-1] + tri[r][c], DP[r-1][c] + tri[r][c]);
-			}
-		}
-		
-		
-		// 3. 최댓값 찾기
-		int answer = 0;
-		for(int c = 1 ; c <= N ; c++) {
-			answer = Math.max(answer, DP[N][c]);
-		}
-		
-		// 4. 출력
-		System.out.println(answer);
-	}
-		
+        int answer = 0;
+        for(int i = 1 ; i <= n ; i++) {
+            if(answer < tri[n][i]) answer = tri[n][i];
+        }
+
+        System.out.println(answer);
+    }
 }
+
