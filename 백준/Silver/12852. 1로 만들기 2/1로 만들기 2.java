bf31a2767927855a5fcf73e58947a50e6eac09a1
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-01-30 23:58:05 +0900

    [Silver I] Title: 1로 만들기 2, Time: 108 ms, Memory: 19808 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -2,70 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[][] dp;
-	
-	public static void main(String[] args)  throws IOException{
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        
-        //입력값이 1일 경우, 출력하고 메인메소드 바로 종료
-		if(N == 1) {
-			System.out.println(0);
-			System.out.println(1);
-			return;
-		}
-		
-		//그 외의 경우
-        dp = new int[N][2]; //회차와 이전 수를 저장할 dp 배열
-        int turn = 1; //회차
-        //1이 되면 종료 = dp[1]이 초기값이었던 0이 아닐때 종료
-        while(dp[1][0] == 0) {
-        	//첫 회차에서는 이전 수를 확인할 필요가 없다
-        	if(turn == 1) {
-        		cal(turn, N);
-        		turn++;
-        		continue;
-        	}
-        	
-        	//그 이후 회차에서는 해당  회차였던 정수 N을 찾아서 연산 진행
-        	for(int i = 1; i < N ; i++) {
-        		if(dp[i][0] != turn - 1) continue;
-        		cal(turn, i);
-        	}
-        	
-        	//회차 더하기
-        	turn++;
+
+        if(N == 1) {
+            System.out.println(0);
+            System.out.println(1);
+            return;
         }
-        
-        //해당 수하는 수를 스택에 넣기
-        Deque<Integer> s = new ArrayDeque<>();
-        s.offerFirst(1);
-        for(int i = 1; i < turn ; i++) {
-        	s.offerFirst(dp[s.peekFirst()][1]);
+
+        int[] dp = new int[N+1];
+        int[] prev = new int[N+1];
+
+        dp[1] = 0;
+
+        for(int i = 2 ; i < N+1 ; i++) {
+            dp[i] = dp[i-1] + 1;
+            prev[i] = i-1;
+
+            if(i%3 == 0 && dp[i] >= dp[i/3] + 1) {
+
+                dp[i] = dp[i/3] + 1;
+                prev[i] = i/3;
+            }
+
+            if(i%2 == 0 && dp[i] >= dp[i/2] + 1) {
+                dp[i] = dp[i/2] + 1;
+                prev[i] = i/2;
+            }
         }
-        
-        //출력
-        StringBuilder sb = new StringBuilder();
-        sb.append(turn -1 + "\n");
-        for(int i = 0 ; i < turn ; i++) {
-        	sb.append(s.pollFirst() + " ");
+
+        sb.append(dp[N]).append("\n");
+        int idx = N;
+        while(idx != 0) {
+            sb.append(idx).append(" ");
+            idx = prev[idx];
         }
         System.out.println(sb);
-	}
-
-	//연산 메소드
-	private static void cal(int turn, int n) {
-		if(n%3 == 0 && dp[n/3][0] == 0) {
-			dp[n/3][0] = turn;
-			dp[n/3][1] = n;
-		}
-		if(n%2 == 0 && dp[n/2][0] == 0) {
-			dp[n/2][0] = turn;
-			dp[n/2][1] = n;
-		}
-		if(dp[n-1][0] == 0) {
-			dp[n-1][0] = turn;
-			dp[n-1][1] = n;
-		}
-	}
+    }
 }
+
