15e2e503e09b74646536b773a2e01d716352cb2a
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-01-31 13:58:24 +0900

    [Silver III] Title: 퇴사, Time: 80 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -2,26 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		//1. 입력받기
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine()); //N
-		
-		int[][] schedule = new int[N+1][2]; //상담 일정 저장
-		for(int i = 1 ; i <= N ; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			schedule[i][0] = Integer.parseInt(st.nextToken());
-			schedule[i][1] = Integer.parseInt(st.nextToken());
-		}
-		
-		//2. dp
-		int[] dp = new int[N+2];
-		
-		//계산하기
-		for(int i = N ; i > 0 ; i--) {
-			if(schedule[i][0] + i > N+1) dp[i] = dp[i+1];
-			else dp[i] = Math.max(dp[i+1], dp[i+schedule[i][0]] + schedule[i][1]);
-		}
-		System.out.println(dp[1]);
-	}
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] T = new int[N+1];
+        int[] P = new int[N+1];
+
+        for(int i = 1 ; i <= N ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N+1];
+        int answer = 0;
+        for(int i = 1 ; i <= N ; i++) {
+            // 퇴사날까지 상담을 완료할 수 없는 경우
+            if(T[i] + i > N+1) {
+                dp[i] = answer;
+                continue;
+            }
+
+            dp[i] = P[i];
+            for(int j = i-1 ; j > 0 ; j--) {
+                if(T[j] + j > i) dp[i] = Math.max(P[i], dp[i]);
+                else dp[i] = Math.max(dp[j] + P[i], dp[i]);
+            }
+
+            if(dp[i] > answer) answer = dp[i];
+        }
+
+        System.out.println(answer);
+    }
 }
+
