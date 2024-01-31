0c855c7b654f1bc7b2126682ff7ee818be353157
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-01-31 16:53:33 +0900

    [Silver II] Title: 제곱수의 합, Time: 136 ms, Memory: 12376 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -1,32 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args)  throws IOException {
-		// 1. 입력 받기
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        
-        // 2. 제곱수 여부 확인
-        if((Math.pow((long)Math.sqrt(N), 2) == N)){
-        	System.out.println(1);
-        	return;
-        }
-        
-        // 3. 아니라면 DP
-        // 모두 1^2로 표시한다는 가정 하에 i로 초기화
+
         int[] dp = new int[N+1];
+        int num = 1;
         for(int i = 1 ; i <= N ; i++) {
-        	dp[i] = i;
+            if(i == num*num) {
+                dp[i] = 1;
+                num++;
+                continue;
+            }
+            dp[i] = dp[i-1] + 1;
+            int tmp = num - 1;
+            while(tmp > 0) {
+                dp[i] = Math.min(dp[i-tmp*tmp] + dp[tmp*tmp],dp[i]);
+                tmp--;
+            }
         }
 
-        for(int i = 1 ; i <= N ; i++) {
-        	for(int j = 2 ; j*j <= i ; j++) {
-        		dp[i] = Math.min(dp[i], dp[i - j*j]+1);
-        	}
-        }
-        
-        System.out.println(dp[N]); 
-	}
+        System.out.println(dp[N]);
+    }
 }
+
