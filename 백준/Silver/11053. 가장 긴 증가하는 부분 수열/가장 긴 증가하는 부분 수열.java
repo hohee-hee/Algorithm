fe3ffd6c620fc7bbe07e472be360d5b73166b5f0
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-01-31 11:30:40 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 100 ms, Memory: 12384 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,37 +1,33 @@
-
 import java.io.*;
 import java.util.*;
 
-public class Main{
-	
-	public static void main(String[] args)  throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N]; //수열 A
-		int[] DP = new int[N]; //DP 배열
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		//입력받기
-		for(int i = 0 ; i < N ; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		
-		DP[0] = 1; //arr[0]은 최장길이가 1(자기자신)이므로 1 할당
-		int maxDP = 0;
-		int longest = 1;
-		for(int i = 1 ; i < N ; i++) {
-			maxDP = 0;
-			for(int j = 0 ; j < i ; j++) {
-				if(arr[i] > arr[j]) {
-					maxDP = Math.max(maxDP, DP[j]);
-				}
-			}			
-			DP[i] = maxDP + 1;
-			longest = Math.max(longest, DP[i]);
-		}
-		
-		System.out.println(longest);
-		
-	}
+public class Main {
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        if(n == 1) {
+            System.out.println(1);
+            return;
+        };
+        int[] arr = new int[n];
+        int[] len = new int[n];
+        int answer = 0;
+        for(int i = 0 ; i < n ; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            len[i] = 1;
+            for(int j = i-1 ; j >= 0 ; j--) {
+                if(arr[j] < arr[i]) {
+                    len[i] = Math.max(1+ len[j], len[i]);
+                }
+            }
+            if(len[i] > answer) answer = len[i];
+        }
+
+        System.out.println(answer);
+    }
 }
+
