b8dc40eb5cb05ba85d3dbe78fff21a4a34fd17a5
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-02-04 01:39:03 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 80 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -1,24 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		
-		long[] arr = new long[N+1];
-		arr[0] = 0;
-		arr[1] = 1;
-		if(N == 1) { 
-			System.out.println(arr[N]); 
-			return; 
-		}
-		for(int i = 2 ; i <= N ; i++) {
-			arr[i] = arr[i-2]+arr[i-1];
-		}
-		
-		System.out.println(arr[N]);
-	}
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if(n == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        long[] dp = new long[n+1];
+        dp[1] = 1;
+        for(int i = 2 ; i <= n ; i++) dp[i] = dp[i-1] + dp[i-2];
+        System.out.println(dp[n]);
+    }
 }
+
