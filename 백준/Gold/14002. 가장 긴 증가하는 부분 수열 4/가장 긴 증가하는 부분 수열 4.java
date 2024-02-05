69135ea11d5a32de31016be46571e31375e06d77
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-02-05 13:54:54 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 100 ms, Memory: 12212 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -2,55 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-        // 1. 입력 받기
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] A = new int[N];
-        for(int i = 0 ; i < N ; i++) {
-        	A[i] = Integer.parseInt(st.nextToken());
-        }
-		
-        if(N == 1) {
-        	System.out.println(1);
-        	System.out.println(A[0]);
-        	return;
-        }
-		// 2. dp
-		// 2중 for로 가장 긴 배열 찾아서 넣기
+        for(int i = 0 ; i < N ; i++) A[i] = Integer.parseInt(st.nextToken());
+
         int[] dp = new int[N];
         dp[0] = 1;
-        int maxlen = 0;
-        int answer = 0;
-        for(int i = 1 ; i < N ; i++) {
-        	maxlen = 0;
-        	for(int j = 0 ; j < i ; j++) {
-        		if(A[j] < A[i])	maxlen = Math.max(maxlen, dp[j]);
-        	}
-        	dp[i] = maxlen+1;
-        	if(dp[i] > answer) answer = dp[i];
+
+        int len = 0;
+        int maxIdx = 0;
+        for(int i = 0 ; i < N ; i++) {
+            dp[i] = 1;
+            for(int j = i-1 ; j >= 0 ; j--) {
+                if(A[i] > A[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            if(dp[i] > len) {
+                len = dp[i];
+                maxIdx = i;
+            }
         }
-        
-        // 3. 최대 길이
-        int idx = answer;
-        int[] print = new int[N+1];
-        for(int i = N-1 ; i >= 0 ; i--) {
-			if(dp[i] == idx) {
-				print[idx-1] = A[i];
-				idx--;
-			}
-        	
+
+        sb.append(len).append("\n");
+        int[] answer = new int[len+1];
+        int aIdx = len;
+        for(int i = maxIdx ; i >= 0 ; i--) {
+            if(dp[i] == aIdx) {
+                answer[aIdx] = A[i];
+                aIdx--;
+            }
         }
-        
-        StringBuilder sb = new StringBuilder();
-        sb.append(answer + "\n");
-        
-        for(int i = 0 ; i < answer ; i++) {
-        	sb.append(print[i] + " ");
-        }
-        
+
+        for(int i = 1 ; i <= len ; i++) sb.append(answer[i]).append(" ");
         System.out.println(sb);
-	}
+    }
 }
+
