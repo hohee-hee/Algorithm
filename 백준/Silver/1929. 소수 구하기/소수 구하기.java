c30866dce847c099eabc566e1e1088ed3a421586
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-02-07 11:06:13 +0900

    [Silver III] Title: 소수 구하기, Time: 180 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,32 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		boolean[] list = new boolean[M+1];
-		for(int i = 0; i <= M ; i++) {
-			list[i] = true;
-		}
-		list[0] = false;
-		list[1] = false;
-		for(long i = 2 ; i <= M ; i++) {
-				for(long j = i*i ; j <= M ; j+=i) {
-					list[(int)j] = false;
-				}			
-		}
-		StringBuilder sb = new StringBuilder();
-		for(int i = N; i <= M ; i++) {
-			if(list[i])
-				sb.append(i).append("\n");
-		}
-		
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        boolean[] sieve = new boolean[N + 1];
+        for (int i = 2; i <= N; i++) {
+            if (sieve[i]) continue;
+            for (int j = 1; j * i <= N; j++) {
+                if (sieve[j * i]) continue;
+                sieve[j * i] = true;
+            }
+
+            if (i >= M) sb.append(i).append("\n");
+        }
+
+        System.out.println(sb);
+    }
 }
