bc339aa1f755df435c2e82e2c46cbb629531c783
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-02-07 11:19:06 +0900

    [Bronze I] Title: 소인수분해, Time: 116 ms, Memory: 11940 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -8,32 +8,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         if(N == 1) return;
-        
-        boolean[] sieve = new boolean[N + 1];
-        sieve[1] = true;
-        for (int i = 2; i*i <= N; i++) {
-            if (sieve[i]) continue;
-            for (int j = i*i; j <= N; j+= i) {
-                sieve[j] = true;
-            }
-        }
-
-        if(!sieve[N]) {
-            System.out.println(N);
-            return;
-        }
 
         for(int i = 2 ; i <= N ; i++) {
-            if(sieve[i]) continue;
-
             while(N % i == 0) {
                 sb.append(i).append("\n");
                 N /= i;
-            }
-
-            if(!sieve[N]) {
-                sb.append(N).append("\n");
-                break;
             }
         }
 
