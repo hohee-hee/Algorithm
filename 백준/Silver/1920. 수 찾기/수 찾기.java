a5e11cf4a057899c39b5595944d5b9e36f3a4cd1
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-02-10 23:49:09 +0900

    [Silver IV] Title: 수 찾기, Time: 504 ms, Memory: 53544 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,23 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         StringBuilder sb = new StringBuilder();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        HashSet<Integer> hs = new HashSet<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0 ; i < N ; i++){
-            hs.add(Integer.parseInt(st.nextToken()));
-        }
 
+        // 1. 입력 받기
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N ; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.parallelSort(arr);
+        // 2. 찾기
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for(int i = 0 ; i < M ; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            if(hs.contains(num)) sb.append(1 + "\n");
-            else sb.append(0 + "\n");
+        find: for(int i = 0 ; i < M ; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            int sp = 0;
+            int ep = N-1;
+            int mid = (ep+sp) / 2;
+
+            while(sp <= ep) {
+                if(target == arr[mid]) {
+                    sb.append(1).append("\n");
+                    continue find;
+                }
+
+                if(arr[mid] > target) {
+                    ep = mid-1;
+                } else {
+                    sp = mid+1;
+                }
+
+                mid = (ep+sp) / 2;
+            }
+            sb.append(0).append("\n");
         }
         System.out.println(sb);
     }
