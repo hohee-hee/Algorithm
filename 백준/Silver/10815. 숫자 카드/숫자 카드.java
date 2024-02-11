87a148f0b548a082ffd6bc1b269433b2d04447c4
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-02-11 16:03:14 +0900

    [Silver V] Title: 숫자 카드, Time: 700 ms, Memory: 153332 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,4 +1,4 @@
-// Arrays.binarySearch 사용
+// boolean 배열 사용
 
 import java.io.*;
 import java.util.*;
@@ -9,15 +9,18 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        boolean[] cards = new boolean[20000001];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0 ; i < N ; i++) arr[i] = Integer.parseInt(st.nextToken());
-        Arrays.parallelSort(arr);
+        for(int i = 0 ; i < N ; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            cards[num+10000000] = true;
+        }
+
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for(int i = 0 ; i < M ; i++) {
             int target = Integer.parseInt(st.nextToken());
-            if(Arrays.binarySearch(arr, target) >= 0) sb.append(1).append(" ");
+            if(cards[target+10000000]) sb.append(1).append(" ");
             else sb.append(0).append(" ");
         }
         System.out.println(sb);
