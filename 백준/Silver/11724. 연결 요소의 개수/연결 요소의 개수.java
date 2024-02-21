0a8dc619865437b9812bd27fa10afff389310f14
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-02-21 21:46:45 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 556 ms, Memory: 144072 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,47 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        ArrayList<Integer>[] adj = new ArrayList[N + 1];
-        for (int i = 0; i < N + 1; i++) adj[i] = new ArrayList<>();
-        for (int i = 0; i < M; i++) {
+        ArrayList<Integer>[] graph = new ArrayList[N+1];
+        for(int i = 1 ; i <= N ; i++) graph[i] = new ArrayList<>();
+
+        for(int i = 0 ; i < M ; i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            adj[u].add(v);
-            adj[v].add(u);
+            graph[u].add(v);
+            graph[v].add(u);
         }
 
+        boolean[] isVisited = new boolean[N+1];
         int answer = 0;
-
-        boolean[] isVisited = new boolean[N + 1];
-        for (int i = 1; i < N + 1; i++) {
+        for(int i = 1 ; i <= N ; i++) {
             if(isVisited[i]) continue;
 
             ArrayDeque<Integer> q = new ArrayDeque<>();
             q.offerLast(i);
             isVisited[i] = true;
-            while (!q.isEmpty()) {
-                int node = q.pollFirst();
+            while(!q.isEmpty()) {
+                int cn = q.pollFirst();
 
-                for(int j = 0 ; j < adj[node].size() ; j++) {
-                    int next = adj[node].get(j);
-
-                    if(isVisited[next]) continue;
-
-                    isVisited[next] = true;
-                    q.offerLast(next);
+                for(int j = 0 ; j < graph[cn].size(); j++) {
+                    int tn = graph[cn].get(j);
+                    if(isVisited[tn]) continue;
+                    q.offerLast(tn);
+                    isVisited[tn] = true;
                 }
             }
 
             answer++;
         }
+
         System.out.println(answer);
     }
 }
