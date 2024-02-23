cc8021553f2d9d69d69e138798ad2b8b019c398c
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-02-23 16:07:48 +0900

    [Silver I] Title: 트리 순회, Time: 76 ms, Memory: 11576 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,72 +1,60 @@
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
+public class Main {
+    public static class Node {
+        char node;
+        char l;
+        char r;
 
-public class Main{
-	
-	static char[][] tree;
-	static int N;
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine()); 
-		tree = new char[N][3];
-		
-		for(int i = 0 ; i < N ; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int j = 0 ; j < 3 ; j++)
-				tree[i][j] = st.nextToken().charAt(0);
-		}
-		preorder(tree[0][0]);
-		sb.append("\n");
-		inorder(tree[0][0]);
-		sb.append("\n");
-		postorder(tree[0][0]);
-		
-		System.out.println(sb);
-	}
+        Node(char node, char l, char r) {
+            this.node = node;
+            this.l = l;
+            this.r = r;
+        }
+    }
+    public static StringBuilder sb = new StringBuilder();
+    public static Node[] graph;
 
-	static void preorder(char ch) {
-		//base case
-		if(ch == '.') return;	
-		
-		//recursive case
-		for(int i = 0 ; i < N ; i++) {
-			if(tree[i][0] == ch) { 
-				sb.append(ch);
-				preorder(tree[i][1]); //left node
-				preorder(tree[i][2]); //right node	
-			}
-		}			
-	}
-	
-	static void inorder(char ch) {
-		//base case
-		if(ch == '.') return;
-		
-		//recursive case
-		for(int i = 0 ; i < N ; i++) {
-			if(tree[i][0] == ch) { 
-				inorder(tree[i][1]); //left node
-				sb.append(ch);
-				inorder(tree[i][2]); //right node	
-			}			
-		}
-	}
-	
-	static void postorder(char ch) {
-		//base case
-		if(ch == '.') return;
-		//recursive case
-		for(int i = 0 ; i < N ; i++) {
-			if(tree[i][0] == ch) { 
-				postorder(tree[i][1]); //left node
-				postorder(tree[i][2]); //right node	
-				sb.append(ch);
-			}
-		}
-		
-	}
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        graph = new Node[N];
+        StringTokenizer st;
+        for(int i = 0 ; i < N ; i++) {
+            st = new StringTokenizer(br.readLine());
+            char node = st.nextToken().charAt(0);
+            char l = st.nextToken().charAt(0);
+            char r = st.nextToken().charAt(0);
+
+            graph[node-'A'] = new Node(node, l, r);
+        }
+
+        preorder(graph[0]);
+        sb.append("\n");
+        inorder(graph[0]);
+        sb.append("\n");
+        postorder(graph[0]);
+
+        System.out.println(sb);
+    }
+
+    public static void preorder(Node curr){
+        sb.append(curr.node);
+        if(curr.l != '.') preorder(graph[curr.l - 'A']);
+        if(curr.r != '.') preorder(graph[curr.r - 'A']);
+    }
+
+    public static void inorder(Node curr){
+        if(curr.l != '.') inorder(graph[curr.l - 'A']);
+        sb.append(curr.node);
+        if(curr.r != '.') inorder(graph[curr.r - 'A']);}
+
+    public static void postorder(Node curr){
+        if(curr.l != '.') postorder(graph[curr.l - 'A']);
+        if(curr.r != '.') postorder(graph[curr.r - 'A']);
+        sb.append(curr.node);
+    }
 }
