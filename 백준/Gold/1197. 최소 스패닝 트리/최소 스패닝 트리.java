d78c3e7be5b151c6bc3661f28f3f95208a65031e
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-02-28 17:01:07 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 468 ms, Memory: 46684 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,68 +1,63 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 
 public class Main {
-	
-	static int[] p;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		//1. 입력 받기
-		int V = Integer.parseInt(st.nextToken()); //정점 개수
-		int E = Integer.parseInt(st.nextToken()); //간선 개수
-		
-		PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+    public static int[] parent, root, rank;
+    public static void main(String[] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
 
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[2] - o2[2];
-			}
-		});
-		
-		int[][] edges = new int[E][3];
-		
-		for(int i = 0 ; i <  E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			int W = Integer.parseInt(st.nextToken());
-			
-			pq.offer(new int[] {A,B,W});
-		}
-		
-		//2. makeset
-		p = new int[V+1];
-		for(int i = 1 ; i <= V ; i++) p[i] = i;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
         
-		//3. 순회
-		int answer = 0;
-		int pick = 0;
-		for(int i = 0 ; i < E ; i++) {
-			int x = pq.peek()[0];
-			int y = pq.peek()[1];
-			int w = pq.poll()[2];
-			
-			if(findset(x) != findset(y)) {
-				union(x,y);
-				answer += w;
-				pick++;				
-			}
-		}
-		
-		System.out.println(answer);
-	}
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[2] - o2[2];
+            }
+        });
+        
+        for(int i = 0 ; i < E ; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+            pq.offer(new int[]{A,B,C});
+        }
 
-	private static void union(int x, int y) {
-		p[findset(y)] = findset(x);
-	}
 
-	private static int findset(int x) {
-		if(x == p[x]) return x;
-		return findset(p[x]);
-	}
-	
-	
+        root = new int[V+1];
+        rank = new int[V+1];
+        for(int i = 1 ; i <= V ; i++) root[i] = i;
+        int answer = 0;
+        while(!pq.isEmpty()) {
+            int u = pq.peek()[0];
+            int v = pq.peek()[1];
+            int w = pq.poll()[2];
+
+            boolean isUnited = union(u, v);
+            if(isUnited) answer += w;
+        }
+
+        System.out.println(answer);
+    }
+
+    public static int find(int x){
+        if(root[x] == x) return x;
+        else return root[x] = find(root[x]);
+    }
+
+    public static boolean union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if(rootX == rootY) return false;
+
+        if(rank[rootX] < rank[rootY]) root[rootX] = rootY;
+        else root[rootY] = rootX;
+
+        if(rank[rootX] == rank[rootY]) rank[rootX]++;
+        return true;
+    }
 }
