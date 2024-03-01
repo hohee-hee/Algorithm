b0b81207ac7e306fe4f34c71278097ae6265871d
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-03-01 23:54:18 +0900

    [Gold IV] Title: 최단경로, Time: 676 ms, Memory: 114892 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,9 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static final int INF = 100_000_000;
-        
-    public static void main(String[] args) throws IOException {
+    public static final int INF = 30000002;
+    public static void main(String[] args) throws IOException{
         StringBuilder sb = new StringBuilder();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,54 +13,48 @@ public class Main {
 
         int K = Integer.parseInt(br.readLine());
 
-        ArrayList<int[]>[] adj = new ArrayList[V+1];
-        for(int i = 0 ; i < V+1 ; i++) adj[i] = new ArrayList<int[]>();
-
+        ArrayList<int[]>[] graph = new ArrayList[V+1];
+        for(int i = 1 ; i <= V ; i++) graph[i] = new ArrayList<>();
         for(int i = 0 ; i < E ; i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            adj[u].add(new int[]{v,w});
+            graph[u].add(new int[]{v,w});
         }
 
-        // 다익스트라
-        int[] dist = new int[V+1];
-        Arrays.fill(dist, INF);
-        dist[K] = 0;
+        int[] answer = new int[V+1];
+        Arrays.fill(answer, INF);
 
         PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                return o1[1] - o2[1];
+                return o1[0] - o2[0];
             }
         });
 
-        pq.add(new int[]{K, dist[K]});
-
+        pq.offer(new int[]{0, K});
         while(!pq.isEmpty()) {
-            int n = pq.peek()[0];
-            int w = pq.poll()[1];
+            int dist = pq.peek()[0];
+            int target = pq.poll()[1];
+            if(answer[target] > dist) answer[target] = dist;
 
-            for(int i = 0 ; i < adj[n].size() ; i++) {
-                int next = adj[n].get(i)[0];
-                int weight = adj[n].get(i)[1] + dist[n];
-
-                if(dist[next] < weight) continue;
-                if(dist[next] > weight) {
-                    dist[next] = weight;
-                    pq.offer(new int[]{next, dist[next]});
+            for(int i = 0 ; i < graph[target].size() ; i++) {
+                int nextNode = graph[target].get(i)[0];
+                int nextDist = graph[target].get(i)[1] + answer[target];
+                if(answer[nextNode] > nextDist) {
+                    answer[nextNode] = nextDist;
+                    pq.offer(new int[]{answer[nextNode], nextNode});
                 }
+
             }
-
         }
 
-        for(int i = 1; i < V + 1 ; i++) {
-            if(dist[i] == INF) sb.append("INF");
-            else sb.append(dist[i]);
-            
-            if(i != V) sb.append("\n");
+        for(int i = 1 ; i <= V; i++) {
+            if(answer[i] >= INF)sb.append("INF\n");
+           else sb.append(answer[i]).append("\n");
         }
+
         System.out.println(sb);
     }
 }
