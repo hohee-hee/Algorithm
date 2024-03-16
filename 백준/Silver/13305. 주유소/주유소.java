3406c4f781386b11365ab98ba3b843624d44fde7
Sohee Kwon <122425990+hohee-hee@users.noreply.github.com>
2024-03-16 23:18:25 +0900

    [Silver III] Title: 주유소, Time: 548 ms, Memory: 52716 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -8,30 +8,33 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int[] dist = new int[N];
+        long[] dist = new long[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 1 ; i < N ; i++) dist[i] = Integer.parseInt(st.nextToken()) + dist[i-1];
 
-        int[][] info = new int[N][3];
+        long[][] info = new long[N][3];
         int[] price = new int[N];
         st = new StringTokenizer(br.readLine());
         for(int i = 0 ; i < N ; i++) {
             price[i] = Integer.parseInt(st.nextToken());
-            info[i] = new int[]{price[i], i, dist[N-1]-dist[i]};
+            info[i] = new long[]{price[i], i, dist[N-1]-dist[i]};
         }
 
-        Arrays.parallelSort(info, new Comparator<int[]>() {
+        Arrays.parallelSort(info, new Comparator<long[]>() {
             @Override
-            public int compare(int[] o1, int[] o2) {
-                if(o1[0] == o2[0]) return o1[1] - o2[1];
-                return o1[0] - o2[0];
+            public int compare(long[] o1, long[] o2) {
+                if(o1[0] == o2[0]) {
+                    if(o1[1] < o2[1]) return -1;
+                    return 1;
+                } else if(o1[0] < o2[0]) return -1;
+                else return 1;
             }
         });
 
         long answer = 0;
-        int curr = N;
+        long curr = N;
         int pt = 0;
-        int prevDist = 0;
+        long prevDist = 0;
         while(curr > 0) {
             if(info[pt][1] > curr) {
                 pt++;
